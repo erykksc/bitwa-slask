18fda63c8a6b1d1dfcffc93dd9154cc6c47fa1cb
eroar <48735008+Eroar@users.noreply.github.com>
2022-05-06 16:45:49 +0200

    Content update

--- src/AboutUs.tsx
+++ src/AboutUs.tsx
@@ -5,10 +5,27 @@ export default function AboutUs({ id, className }: SectionProps): JSX.Element {
         <section id={id} className={`page-section row ${className}`}>
             <div className='col-12 col-md-6'>
                 <h2>O nas</h2>
-                <p style={{ textAlign: 'justify' }}>Quis laboris sunt officia Lorem esse occaecat eu do voluptate. Anim excepteur sit amet nisi ullamco labore qui. Veniam elit deserunt anim ad incididunt aliquip ut aliquip qui aute mollit non laborum. Irure occaecat aliquip ea magna ex eu officia do excepteur officia. Cupidatat duis aliqua labore tempor. Ipsum officia nostrud velit enim magna elit. Quis consectetur veniam eiusmod eu officia ea laborum eu fugiat proident.Lorem aliqua nulla deserunt id adipisicing Lorem do voluptate adipisicing velit eiusmod culpa id irure. Sunt officia in dolore est. Officia officia veniam esse commodo incididunt velit commodo tempor exercitation non. Cupidatat aute nisi tempor pariatur id cupidatat reprehenderit velit ipsum anim laborum dolor Lorem laboris. Fugiat nulla laborum Lorem veniam duis. Aliquip qui minim incididunt tempor exercitation reprehenderit. In fugiat adipisicing in eu proident ea sit officia aliqua cupidatat sunt nisi.</p>
+                <div style={{ textAlign: 'justify' }}>
+                    <p>
+                        Z pasji jaką jest boks i setek godzin spędzonych na sali treningowej
+                        poszliśmy o krok dalej i postanowiliśmy promować nasza ukochaną
+                        dyscyplinę sportową. Biorąc pod uwagę olbrzymie tradycje bokserskie jakie
+                        posiada Śląsk narodził się pomysł stworzenia cyklicznej imprezy
+                        <strong> "Bitwa o Śląsk"</strong>, gdzie boks olimpijski przeplata się wraz z boksem zawodowym.
+                    </p>
+                    <p>
+                        Dzieki takiemu połączeniu to wydarzenia sportowe nabiera na atrakcyjności zarówno
+                        dla samych zawodników jak i kibiców.
+                        Nasze gale odbywały się już w takich miastach jak <strong>Katowice, Siemianowice&nbsp;Śląskie,
+                        Radzionków, Zabrze, Mysłowice</strong> gdzie mieliśmy przyjemność współpracować z władzami
+                        miasta a nasze imprezy są wizytówka boksu w regionie i całym kraju oraz poza granicami.
+                        Na nasze imprezy przyjeżdżają się mierzyć zarówno zawodnicy z całej Polski jak również
+                        <strong> Ukrainy, Moldawi czy Rosji</strong>.
+                    </p>
+                </div>
             </div>
             <div className='col-12 col-md-6 d-flex justify-content-center'>
-                <img src='img/ring.png' style={{ objectFit: 'contain', maxWidth: '100%', height: 'auto' }} alt="Ring"/>
+                <img src='img/ring.png' style={{ objectFit: 'contain', maxWidth: '100%', height: 'auto' }} alt="Ring" />
             </div>
         </section>
     );
