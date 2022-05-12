1dad058cfa077cae2eb256fddc7e67ce22eda66f
eroar <48735008+Eroar@users.noreply.github.com>
2022-05-12 21:57:23 +0200

    Add --delete to rsync in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 npm run build
-rsync --progress -ra ./build/ h1022714@94.152.11.197:~/public_html/bitwaoslask.pl/
+rsync --progress --delete -ra ./build/ h1022714@94.152.11.197:~/public_html/bitwaoslask.pl/
