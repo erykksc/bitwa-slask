d3c9799f2686d9b885c15490d807b81a2e982f12
eroar <48735008+Eroar@users.noreply.github.com>
2022-06-08 14:57:34 +0200

    Update stream link to facebook event page

--- src/NextEvent.tsx
+++ src/NextEvent.tsx
@@ -16,7 +16,7 @@ function InfoBox({ title, icon, children }: { title: string, icon?: string, chil
 }
 
 export default function NextEvent({ id }: SectionProps) {
-    const streamHref = 'https://youtube.com';
+    const streamHref = 'https://fb.me/e/4Wb5VjPA4';
     return (
         <section id={id}>
             <h2 className='text-center'>Bitwa o Śląsk Katowice 17.06.2022</h2>
