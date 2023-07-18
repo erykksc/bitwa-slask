982456208759d36bdc3f925cf9c979d5c6fe7961
eroar <48735008+Eroar@users.noreply.github.com>
2023-07-18 20:51:59 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,6 @@
 #~/bin/bash
 npm run build
 
-if [ "$1" = "eryk.site" ] 
-then
-    dest="eryk.site:~/domains/eryk.site/public_html/bitwaoslask"
-else
-    dest="h1022714@94.152.11.197:~/public_html/bitwaoslask.pl/"
-fi
-rsync --progress --delete -ra ./build/ $dest
+dest="eryk.site:~/domains/bitwaoslaskboxing.pl/public_html/"
+echo "Deploying to $dest"
+rsync --human-readable --info=progress2 --delete -ra ./build/ $dest
